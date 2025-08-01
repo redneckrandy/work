f6678025a7e62c9896cb3dd56a47ec7c52ec7550
redneckrandy <brad@redneckrandy.net>
2025-08-01 18:10:22 +0000

    initial sync

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ AI Runtime Live on Railway."
+    return "<h1>Hello Brad</h1><p>System is live and connected.</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
